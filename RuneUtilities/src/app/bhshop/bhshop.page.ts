ffe4e44fb86e85a3439789edb9c58c9554b67db7
AnthonySams77 <42755588+AnthonySams77@users.noreply.github.com>
2019-04-12 15:31:23 -0500

    almost complete bhshop

--- RuneUtilities/src/app/bhshop/bhshop.page.ts
+++ RuneUtilities/src/app/bhshop/bhshop.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemDataService } from '../item-data.service';
 
 @Component({
   selector: 'app-bhshop',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BHShopPage implements OnInit {
   itemsArray = [];
-  constructor() { }
+  constructor(private itemDataService:ItemDataService) { 
+    
+  }
 
   ngOnInit() {
+    this.itemDataService.getBHItemInfo();
   }
 
 }
--- RuneUtilities/src/app/item-data.service.ts
+++ RuneUtilities/src/app/item-data.service.ts
@@ -2,7 +2,6 @@ import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from "rxjs/operators";
-import { resolve } from 'path';
 
 @Injectable({
   providedIn: 'root'
@@ -31,7 +30,7 @@ export class ItemDataService implements OnInit {
       map((resp: Response) => resp.json().then(
         res => {
           this.itemArray.concat(res.json);
-          resolve();
+
         }
       ))
     )
